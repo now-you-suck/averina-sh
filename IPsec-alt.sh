358939e565bb1e69da58613290ffec03d5a8c490
now-you-suck <23.01.2006.kolya@gmail.com>
2026-05-26 01:15:57 +0300

    Add VPN configuration using NetworkManager and strongSwan

--- IPsec-alt.sh
+++ IPsec-alt.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 apt-get update && apt-get install NetworkManager-strongswan strongswan strongswan-charon-nm strongswan-testing
 cp /home/user/Загрузки/root-ca.crt /etc/pki/ca-trust/source/anchors/ && update-ca-trust
+CERT_PATH="/etc/pki/ca-trust/source/anchors/root-ca.crt"
+
+nmcli connection add \
+  type vpn \
+  vpn-type strongswan \
+  con-name "vpn" \
+  ifname "ens18" \
+  vpn.secrets "password=P@ssw0rdP@ssw0rd"
+  vpn.data "address=10.0.0.1, \
+            certificate=$CERT_PATH, \
+            esp = aes256-sha256-modp2048,\
+            ike = aes256-sha256-modp2048, \
+            method=eap, \
+            user=Administrator, \
+            virtual=yes"
+            
+nmcli connection up "vpn"
